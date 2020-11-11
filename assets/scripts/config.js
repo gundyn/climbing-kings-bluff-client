7aed618fb2bb5019320096750d8efb84c2350ab3
gundyn <nathan.gundy@gmail.com>
2020-11-11 13:19:54 -0600

    added heroku link in config

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: '<replace-with-heroku-url>',
+  production: 'https://git.heroku.com/infinite-dawn-24722.git',
   development: 'http://localhost:4741'
 }
 
