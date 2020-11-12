46b675a4ff907014813438770b4c5c9dc5c1d6c1
gundyn <nathan.gundy@gmail.com>
2020-11-11 20:29:28 -0600

    heroku link fixed

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: 'https://infinite-dawn-24722.herokuapp.com/',
+  production: 'https://infinite-dawn-24722.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
